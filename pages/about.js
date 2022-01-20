cad29747cfa27d38117a47d770dfa9191b4fee3e
Mahier <mahier.sydow8@gmail.com>
2022-01-20 15:50:03 +0900

    setup/pages and routes/components/layout components

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, non nulla. Odit cupiditate dignissimos veniam sequi vitae totam accusantium deleniti dolore, obcaecati ad rem voluptatum in. Libero veritatis maiores soluta!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, non nulla. Odit cupiditate dignissimos veniam sequi vitae totam accusantium deleniti dolore, obcaecati ad rem voluptatum in. Libero veritatis maiores soluta!</p>
+        </div>
+    );
+}
+ 
+export default About;
